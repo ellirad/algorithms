38faabf0671546c3e964afd6fca0d387955aa66b
elirad <44933419+ellirad@users.noreply.github.com>
2023-08-17 15:49:36 +0330

    solve with o(n) time complexity

--- productExceptSelf.js
+++ productExceptSelf.js
@@ -5,18 +5,54 @@
 
 // Need a function to multiply numbers.
 
-var productExceptSelf = function(nums) {
-    let ans= []
-    for(let i = 0; i< nums.length; i++) {
-        let _nums = [...nums]
-        _nums[i] = 1;
-        ans.push(productOfNums(_nums))
-    }
+// First solution with time complexity of O(2n) that is not good
 
-    return ans
-};
+// function productExceptSelf(nums) {
+//     let ans= []
+//     for(let i = 0; i< nums.length; i++) {
+//         let _nums = [...nums]
+//         _nums[i] = 1;
+//         ans.push(productOfNums(_nums))
+//     }
 
-function productOfNums(arr) {
-    const product = arr.reduce((a, b) => a* b); 
-    return product
+//     return ans
+// };
+
+// function productOfNums(arr) {
+//     // const product = arr.reduce((a, b) => a* b);
+//     // return product
+//     let product = 1
+//     for (let i = 0; i < arr.length; i++) {
+//         product *= arr[i]
+//     }
+//     return product
+// }
+
+// Second approach with time complexity of O(n)
+
+function productExceptSelf(nums) {
+  let left = new Array(nums.length);
+  let right = new Array(nums.length);
+  let ans = new Array(nums.length);
+
+  left[0] = 1;
+  right[nums.length - 1] = 1;
+
+  for (let i = 1; i < nums.length; i++) {
+    left[i] = nums[i - 1] * left[i - 1];
+  }
+
+  for (let i = nums.length - 1; i >= 0; i--) {
+    // len = 4, i = 3
+    right[i - 1] = right[i] * nums[i];
+  }
+
+  for (let i = 0; i < nums.length; i++) {
+    ans[i] = right[i] * left[i];
+  }
+
+  return ans;
 }
+
+console.log(productExceptSelf([2, 5, 7, 3]));
+console.log(productExceptSelf([11, 9, 2, 1]));
