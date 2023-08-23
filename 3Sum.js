c31bc73638978640744a12a4a6e5434ead966b40
elirad <44933419+ellirad@users.noreply.github.com>
2023-08-23 23:54:32 +0330

    change 3sum algo

--- 3Sum.js
+++ 3Sum.js
@@ -3,46 +3,32 @@
  * @return {number[][]}
  */
 
- /*
- 1. check the length of nums, if its 3 and sum is more than zero return an empty array as answer.
- 2. divide the lenght of array into 3 to get the lenght of answer.
- 3. create an array with 3 zero: [0, 0 , 0].
- 4. pick one random number from nums, then pick second, check the euality condition and if it's not true pick another, if true split those numbers from nums. repeat 3 in order to find the right answer.
- 5. create a variable to set your prefered and third number, first search for that number in nums, then replace first item with the given array with two condition:     A. not equal with the two   B. sum is zero.
- 6. repeat 4 for the second and third if do not find the triplets.
- 7. check the length of answer if it is equal to number 2 we're done.
- 8. repeat 1 to 7.  
- */
+var threeSum = function (nums) {
+  let sortedNums = nums.sort((a, b) => a - b);
+  let ans = [];
+  let ansIndex = [];
 
- var threeSum = function(nums) {
-    let answer = [];
-    let lenghtOfAnswer = Math.floor(answer.length / 3);
-    
-    if(nums.length === 3) {
-        if(checkSum(nums)) return nums;
-        else return answer;
+  for (let i = 2; i < sortedNums.length; i++) {
+    let temp = [sortedNums[i - 2], sortedNums[i - 1]];
+    let sumOfTwo = sortedNums[i - 2] + sortedNums[i - 1];
+    let ansIndex = [i - 2, i - 1];
+
+    for (let j = 0; j < sortedNums.length; j++) {
+      console.log(sumOfTwo + sortedNums[j]);
+      if (sumOfTwo + sortedNums[j] === 0) {
+        if(j !== i - 2 || j !== i - 1)
+        temp.push(sortedNums[j]);
+      console.log(temp, 'temp')
+      }
     }
 
-    let temp = [sums[0] , sums[1], 0];
+    if (temp.length === 3) ans.push(temp);
+  }
 
-    if(sums.find(num => num === preferedNum)) { 
-        temp.push(preferedNum);
-
-        if(lenghtOfAnswer === temp.length) return answer.push(temp);
-    }
-
-
-    
-    function preferedNum () {
-        let _num;
-        if(sums[0] + sums[1] + _num === 0) return _num 
-    }
-
-    function checkSum(array) {
-     const sum = array.reduce((partialSum, a) => partialSum + a, 0);
-     if(sum === 0) return true;
-     else return false
-    }
-
-
+  return ans;
 };
+
+// console.log(threeSum([-1, 0, 1, 2, -1, -4]));
+// console.log(threeSum([1, 1, -2])); // [[-2,1,1]]
+// console.log(threeSum([-2, 0, 1, 1, 2])); // [[-2,0,2],[-2,1,1]]
+console.log(threeSum([0, 0, 0]));
