8d148f1a1c49ddcaeefebc9a92ee8556e12eb0e1
elirad <44933419+ellirad@users.noreply.github.com>
2023-08-27 02:47:07 +0330

    with hashmap

--- twoSum.js
+++ twoSum.js
@@ -3,20 +3,32 @@
  * @param {number} target
  * @return {number[]}
  */
+// function twoSum(nums, target) {
+//  let map = new Map();
+
+//  for(let i = 0; i < nums.length; i++) {
+//   let diff = target - nums[i];
+
+//   if(map.has(diff)) {
+//     return [i, map.get(diff)]
+//   }
+
+//   map.set(nums[i], i)
+//  }
+// }
+
+// With hash map
 function twoSum(nums, target) {
- let map = new Map();
-
- for(let i = 0; i < nums.length; i++) {
-  let diff = target - nums[i];
-  
-  if(map.has(diff)) {
-    return [i, map.get(diff)]
+  let hashmap = {};
+  for (let i = 0; i < nums.length; i++) {
+    let diff = target - nums[i];
+    if (diff in hashmap) {
+      return [nums.indexOf(diff), i];
+    } else if(nums.indexOf(diff) !== -1) {
+      hashmap[nums[i]] = i  
+    }
   }
-
-  map.set(nums[i], i)
- }
 }
-
 
 console.log(twoSum([2, 7, 11, 15], 9));
 console.log(twoSum([3, 2, 4], 6));
