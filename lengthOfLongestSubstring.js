f13402c6c79ecff2e371ec43780d0d0345f73d50
elirad <44933419+ellirad@users.noreply.github.com>
2023-09-05 01:39:47 +0330

    longest substring

--- lengthOfLongestSubstring.js
+++ lengthOfLongestSubstring.js
@@ -2,16 +2,29 @@
  * @param {string} s
  * @return {number}
  */
-const lengthOfLongestSubstring = function(s) {
+const lengthOfLongestSubstring = function (s) {
     let left = 0;
-    let right = 1;
-    let hashMap =new Set();
+    let right = 0;
+    let maxLength = 0;
+    let hashMap = new Set();
 
-    while (left < right && right < s.length) {
-        if(!hashMap.has(s[left])) {
-            hashMap.set(s[left]);
-            right++
+    for (let i = 0; i < s.length; i++) {
+        if (!hashMap.has(s[i])) {
+            hashMap.add(s[i]);
+            maxLength = Math.max(maxLength, hashMap.size)
+        } else {
+            while (s[left] !== s[i]) {
+                hashMap.delete(s[left]);
+                left++
+            }
+            left++
         }
+        right++
     }
 
+    return maxLength;
 };
+
+console.log(lengthOfLongestSubstring('abcabcbb'))
+console.log(lengthOfLongestSubstring('bbbbbbb'))
+console.log(lengthOfLongestSubstring('pwwkew'))
