b26ee447ed293bee050d1b715beefccac9023375
elirad <44933419+ellirad@users.noreply.github.com>
2023-09-10 20:01:22 +0330

    reverse in place

--- reverseString.js
+++ reverseString.js
@@ -1,11 +1,19 @@
 function reverseString(s) {
-    let reversed = new Array(s.length).fill(null);
+    // let reversed = new Array(s.length).fill(null);
+    // let len = s.length - 1;
+    // for(let i = 0; i < s.length ; i++) {
+    //     reversed[len - i ] = s[i];
+    // }
+    //
+    // return reversed;
     let len = s.length - 1;
-    for(let i = 0; i < s.length ; i++) {
-        reversed[len - i ] = s[i];
+    for(let i = 0; i < s.length / 2; i++) {
+        let temp = s[i];
+        let temp2 = s[len - i];
+        s[len - i ] = temp;
+        s[i] = temp2
     }
-
-    return reversed;
+    return s
 }
 
 console.log(reverseString(["h","e","l","l","o"]))
