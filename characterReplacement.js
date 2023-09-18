02d7a6cca2febf444efe34ee3f1d74e243510d81
elirad <44933419+ellirad@users.noreply.github.com>
2023-09-18 22:20:37 +0330

    second solution

--- characterReplacement.js
+++ characterReplacement.js
@@ -1,29 +1,49 @@
-function characterReplacement(s, k) {
-    let start = 0
-    let end = 0;
-    let maxLength = 0;
-    let maxCount = 0;
-    let freq = {};
+// function characterReplacement(s, k) {
+//     let start = 0
+//     let end = 0;
+//     let maxLength = 0;
+//     let maxCount = 0;
+//     let freq = {};
+//
+//     for (let i = 0; i < s.length; i++) {
+//         let char = s[i];
+//
+//         freq[char] = (freq[char] || 0) + 1;
+//
+//         maxCount = Math.max(maxCount, freq[char]);
+//
+//         if (end - start + 1 - maxCount <= k) {
+//             maxLength = Math.max(maxLength, end - start + 1);
+//         } else {
+//             freq[s[start]]--;
+//             start++;
+//         }
+//
+//         end++;
+//     }
+//
+//     return maxLength;
+// }
 
-    for (let i = 0; i < s.length; i++) {
-        let char = s[i];
+var characterReplacement = function(s, k) {
+    let res = 0;
+    let count = new Map();
+    let l = 0;
 
-        freq[char] = (freq[char] || 0) + 1;
+    for (let r = 0; r < s.length; r++) {
+        let len  = r - l + 1
+        count.set(s[r], 1 + (count.get(s[r]) || 0))
 
-        maxCount = Math.max(maxCount, freq[char]);
-
-        if (end - start + 1 - maxCount <= k) {
-            maxLength = Math.max(maxLength, end - start + 1);
-        } else {
-            freq[s[start]]--;
-            start++;
+        if ((len - Math.max(...count.values())) > k) {
+            count.set(s[l], count.get(s[l]) - 1)
+            l++;
         }
-
-        end++;
+        len = r - l + 1;
+        res = Math.max(res, len)
     }
 
-    return maxLength;
-}
+    return res;
+};
 
 console.log(characterReplacement('ABAB', 2))
 console.log(characterReplacement('AABABBA', 1))
