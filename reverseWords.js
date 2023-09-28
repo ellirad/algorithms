81c25463c89c06951407045f07b8ba146f4a8987
elirad <44933419+ellirad@users.noreply.github.com>
2023-09-28 21:11:06 +0330

    solve in o(n)

--- reverseWords.js
+++ reverseWords.js
@@ -3,19 +3,23 @@
 function reverseWords(string) {
     let newStr = string.split('');
     let result = [];
+    let res = []
     let reversedWord = ''
 
     for (let j = newStr.length - 1; j >= 0; j--) {
         if(newStr[j] === ' ') {
-            result.unshift(reversedWord);
+            result.push(reversedWord);
             reversedWord = ''
         } else if(j === 0) {
             reversedWord+= newStr[j];
-            result.unshift(reversedWord);
+            result.push(reversedWord);
         }
         else reversedWord+= newStr[j];
     }
-    return result.join(' ')
+    for (let i = result.length - 1; i >= 0; i--) {
+        res.push(result[i])
+    }
+    return res.join(' ')
 }
 
 console.log(reverseWords('this is a string'))
