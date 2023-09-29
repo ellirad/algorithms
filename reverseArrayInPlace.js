be9ef01d08bba76e9eacdbab8691ca6267ab65c7
elirad <44933419+ellirad@users.noreply.github.com>
2023-09-29 23:50:08 +0330

    valid prantheses

--- reverseArrayInPlace.js
+++ reverseArrayInPlace.js
@@ -1,10 +1,8 @@
+// Do not push element into a new array and return that array.
+// Do not use array.reverse
+
 function reverseArrayInPlace (arr) {
-    for (let i = 0; i < arr.length / 2; i++) {
-        const temp = arr[i];
-        arr[i] = arr[arr.length - 1 - i];
-        arr[arr.length - 1 - i] = temp;
-    }
-    return arr;
+    
 }
 
 console.log(reverseArrayInPlace([1, 2, 3, 4, 5, 6, 7]));
