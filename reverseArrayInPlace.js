77d8202716b9fa7ae04244fafd3c18b9cc6a19ad
elirad <44933419+ellirad@users.noreply.github.com>
2023-10-03 10:26:09 +0330

    reverse array in place

--- reverseArrayInPlace.js
+++ reverseArrayInPlace.js
@@ -2,7 +2,12 @@
 // Do not use array.reverse
 
 function reverseArrayInPlace (arr) {
-    
+    for (let i = 0; i < arr.length / 2; i++) {
+        let _temp = arr[i];
+        arr[i] = arr[arr.length - 1 - i]
+        arr[arr.length - 1 - i] = _temp
+    }
+    return arr
 }
 
-console.log(reverseArrayInPlace([1, 2, 3, 4, 5, 6, 7]));
+console.log(reverseArrayInPlace([1, 2, 3, 4, 5, 6, 7])); // 7, 6, 5, 4, 3, 2, 1
