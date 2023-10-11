f1a8a1517f17db3c054436c0f61fa2c0efa5c4d5
elirad <44933419+ellirad@users.noreply.github.com>
2023-10-11 17:00:11 +0330

    fib memo

--- fibMemo.js
+++ fibMemo.js
@@ -2,12 +2,11 @@ function fibMemo(index, cache) {
     cache = cache || [];
 
     // Base case
-    if(cache[index]) return cache[index];
+    if (cache[index]) return cache[index];
+    if (index < 3) return 1;
+    cache[index] = fibMemo(index - 1, cache) + fibMemo(index - 2, cache);
 
-    if(index < 3) return 1;
-
-    return fibMemo(index - 1, cache) + fibMemo(index - 2, cache);
-
+    return cache[index]
 }
 
-console.log(fibMemo(20));
+console.log(fibMemo(7));
--- twoSum.js
+++ twoSum.js
@@ -3,6 +3,28 @@
  * @param {number} target
  * @return {number[]}
  */
+
+function twoSum(nums, sum) {
+    let hashTable ={};
+    let result = [];
+    for (let i = 0; i < nums.length; i++) {
+        hashTable[nums[i]] = (hashTable[nums[i]] || 0) + 1
+    }
+    console.log(hashTable)
+
+    for (let i = 0; i < nums.length; i++) {
+        let num1 = sum - nums[i];
+        console.log(hashTable[num1])
+        if(hashTable[num1] > 0) {
+            result.push([nums[i], num1]);
+            hashTable[num1]--
+        } else continue
+    }
+
+    return result
+}
+
+
 // function twoSum(nums, target) {
 //  let map = new Map();
 
@@ -32,18 +54,31 @@
 
 // With two pointer in sorted array. it works only in sorted array.
 // you should move both pointer. right and left
-function twoSum(nums, target) {
-  let left = 0;
-  let right = nums.length - 1;
+// function twoSum(nums, target) {
+//   let left = 0;
+//   let right = nums.length - 1;
+//
+//   while (left < right) {
+//     if (nums[left] + nums[right] === target) return [nums[left], nums[right]];
+//     else if(nums[left] + nums[right] > target) right--;
+//     else if(nums[left] + nums[right] < target) left++;
+//   }
+// }
 
-  while (left < right) {
-    if (nums[left] + nums[right] === target) return [nums[left], nums[right]];
-    else if(nums[left] + nums[right] > target) right--;
-    else if(nums[left] + nums[right] < target) left++;
-  }
-}
 
-console.log(twoSum([2, 7, 11, 15], 9));
-console.log(twoSum([2, 3, 4], 6));
-console.log(twoSum([3, 3], 6));
-console.log(twoSum([10, 20, 35, 50, 75, 80], 70));
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log(twoSum([2, 7, 11, 15], 9));
+console.log(twoSum([2, 3,3, 4], 6));
+// console.log(twoSum([3, 3], 6));
+// console.log(twoSum([10, 20, 35, 50, 75, 80], 70));
