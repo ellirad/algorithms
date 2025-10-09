cdf4bf6dbe3431df241e388946051bd39cccb7de
ellie <44933419+ellirad@users.noreply.github.com>
2025-10-09 22:59:07 +0330

    running sum

---
+++ runningSum.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var runningSum = function(nums) {
+	let sum = 0;
+	for (let i = 0; i < nums.length; i++) {
+		nums[i] = sum + nums[i];
+		sum = nums[i]
+	}
+	return nums
+};
+
+console.log(runningSum([1,2,3,4,5]))
