4c7318e3b0ac0910b7e91fb8e04b9bd5d322b63e
ellie <44933419+ellirad@users.noreply.github.com>
2025-12-03 22:37:50 +0330

    encode string

--- decodeAndEncode.js
+++ decodeAndEncode.js
@@ -6,8 +6,32 @@ function encode(array) {
     return JSON.stringify(hashTable);
 }
 
+function encode2(str, num) {
+	let alphabetic = "abcdefghijklmnopqrstuvwxyz".split("");
+	let newString = "";
+
+	for (let i = 0; i < str.length; i++) {
+		const element = str[i];
+		if(element === ' ') {
+			newString += ' ';
+			continue;
+		}
+
+		let currentIndex = alphabetic.indexOf(element);
+		let newIndex = currentIndex + num;
+
+		if (newIndex > 25) newIndex = newIndex - 26
+		if (newIndex < 0 ) newIndex = 26 + newIndex;
+
+		newString += alphabetic[newIndex];
+	}
+	console.log(newString)
+}
+
 let str = encode(['eat', 'sleep', 'ate', 'ziiiiiiiiiiiiiiiiiiii', '', ';']);
+console.log(str)
 console.log(decode(str));
+encode2('zoo topia', 1)
 
 function decode(str) {
     let ans = [];
