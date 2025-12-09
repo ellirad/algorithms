6db76d870405d469532b1d1ada4aaa1406f99d40
ellie <44933419+ellirad@users.noreply.github.com>
2025-12-09 23:36:57 +0330

    generateParenthesis

---
+++ generateParenthesis.js
@@ -0,0 +1,36 @@
+/*
+22. Generate Parentheses
+Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+Example 1:
+
+Input: n = 3
+Output: ["((()))","(()())","(())()","()(())","()()()"]
+Example 2:
+
+Input: n = 1
+Output: ["()"]
+ */
+
+
+var generateParenthesis = function(n) {
+	let result = [];
+
+	function backtrack(current, open, close) {
+		if (current.length === n * 2) {
+			result.push(current);
+			return;
+		}
+
+		if (open < n) {
+			backtrack(current + "(", open + 1, close);
+		}
+
+		if (close < open) {
+			backtrack(current + ")", open, close + 1);
+		}
+	}
+
+	backtrack("", 0, 0);
+	return result;
+};
